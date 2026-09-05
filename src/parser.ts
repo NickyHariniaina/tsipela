aaa455368ff56c2d8f30a5881fb60958b7789fe0
NickyHariniaina <ferrissushi@gmail.com>
2026-09-05 11:03:34 +0300

    chore: map almost all double letter

--- src/parser.ts
+++ src/parser.ts
@@ -35,16 +35,20 @@ export function separate(
     for (let i = 0; i < syllables.length; i++) {
         if (syllables.length > 0 && syllables.length > i) {
             if (syllables[i]?.syllable == Syllable.consonant) {
-                if (syllables[i]?.character == "t") {
-                    if (syllables[i + 1]?.syllable == Syllable.u) {
-                        syllables[i] = {
-                            syllable: Syllable.tu,
-                            character: "to",
-                        };
-                        syllables.splice(i + 1, 1);
-                        undefinedCount--;
-                    }
-                }
+                separateN(syllables, i, undefinedCount);
+                separateT(syllables, i, undefinedCount);
+                separateR(syllables, i, undefinedCount);
+                separateM(syllables, i, undefinedCount);
+                separateL(syllables, i, undefinedCount);
+                separateH(syllables, i, undefinedCount);
+                separateD(syllables, i, undefinedCount);
+                separateK(syllables, i, undefinedCount);
+                separateZ(syllables, i, undefinedCount);
+                separateG(syllables, i, undefinedCount);
+                separateF(syllables, i, undefinedCount);
+                separateS(syllables, i, undefinedCount);
+                separateB(syllables, i, undefinedCount);
+                separateV(syllables, i, undefinedCount);
             }
         }
     }
@@ -54,4 +58,481 @@ export function separate(
     }
 
     return { syllables };
+}
+
+function separateR(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "r") {
+        if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.ru,
+                character: "ro",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ra,
+                character: "ra",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.ri,
+                character: syllables[i + 1]?.character == "i" ? "ri" : "ry",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.re,
+                character: "re",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateN(
+    syllables: SyllableObject[],
+    i: number,
+    undefinedCount: number,
+) {
+    if (syllables[i]?.character == "n") {
+        if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.ni,
+                character: syllables[i + 1]?.character == "i" ? "ni" : "ny",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.nu,
+                character: "no",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.na,
+                character: "na",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateT(
+    syllables: SyllableObject[],
+    i: number,
+    undefinedCount: number,
+) {
+    if (syllables[i]?.character == "t") {
+        if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.tu,
+                character: "to",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.ti,
+                character: syllables[i + 1]?.character == "i" ? "ti" : "ty",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ta,
+                character: "ta",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateM(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "m") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ma,
+                character: "ma",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.mu,
+                character: "mo",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.mi,
+                character: syllables[i + 1]?.character == "i" ? "mi" : "my",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.me,
+                character: "me",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateL(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "l") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.la,
+                character: "la",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.lu,
+                character: "lo",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.li,
+                character: syllables[i + 1]?.character == "i" ? "li" : "ly",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.le,
+                character: "le",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateH(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "h") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ha,
+                character: "ha",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.hu,
+                character: "ho",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.hi,
+                character: syllables[i + 1]?.character == "i" ? "hi" : "hy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.he,
+                character: "he",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateD(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "d") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.da,
+                character: "da",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.du,
+                character: "do",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.di,
+                character: syllables[i + 1]?.character == "i" ? "di" : "dy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else  if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.de,
+                character: "de",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.da,
+                character: "da",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateK(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "k") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ka,
+                character: "ka",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.ki,
+                character: syllables[i + 1]?.character == "i" ? "ki" : "ky",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.ku,
+                character: "ko",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.ke,
+                character: "ke",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateZ(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "z") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.za,
+                character: "za",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.zi,
+                character: syllables[i + 1]?.character == "i" ? "zi" : "zy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.zu,
+                character: "zo",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.ze,
+                character: "ze",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateG(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "g") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ga,
+                character: "ga",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.gi,
+                character: syllables[i + 1]?.character == "i" ? "gi" : "gy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.gu,
+                character: "go",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.ge,
+                character: "ge",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateF(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "f") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.fa,
+                character: "fa",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.fi,
+                character: syllables[i + 1]?.character == "i" ? "fi" : "fy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.fu,
+                character: "fo",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.fe,
+                character: "fe",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateS(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "s") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.sa,
+                character: "sa",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.si,
+                character: syllables[i + 1]?.character == "i" ? "si" : "sy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.su,
+                character: "so",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.se,
+                character: "se",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateB(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "b") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.ba,
+                character: "ba",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.bi,
+                character: syllables[i + 1]?.character == "i" ? "bi" : "by",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.bu,
+                character: "bo",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.be,
+                character: "be",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
+}
+
+function separateV(syllables: SyllableObject[], i: number, undefinedCount: number) {
+    if (syllables[i]?.character == "v") {
+        if (syllables[i + 1]?.syllable == Syllable.a) {
+            syllables[i] = {
+                syllable: Syllable.va,
+                character: "va",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.i) {
+            syllables[i] = {
+                syllable: Syllable.vi,
+                character: syllables[i + 1]?.character == "i" ? "vi" : "vy",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.u) {
+            syllables[i] = {
+                syllable: Syllable.vu,
+                character: "vo",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        } else if (syllables[i + 1]?.syllable == Syllable.e) {
+            syllables[i] = {
+                syllable: Syllable.ve,
+                character: "ve",
+            };
+            syllables.splice(i + 1, 1);
+            undefinedCount--;
+        }
+    }
 }
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -38,7 +38,7 @@ describe("Parse to get syllable", () => {
         });
     });
 });
-describe("Parse to get consonant syllable", () => {
+describe("Parse to get double letter syllable", () => {
     it("should return correct syllable for eto", () => {
         expect(separate("eto")).toEqual({
             syllables: [
@@ -47,4 +47,180 @@ describe("Parse to get consonant syllable", () => {
             ],
         });
     });
+
+    it("should return correct syllable for etaty", () => {
+        expect(separate("etaty")).toEqual({
+            syllables: [
+                { syllable: Syllable.e, character: "e" },
+                { syllable: Syllable.ta, character: "ta" },
+                { syllable: Syllable.ti, character: "ty" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for eny", () => {
+        expect(separate("eny")).toEqual({
+            syllables: [
+                { syllable: Syllable.e, character: "e" },
+                { syllable: Syllable.ni, character: "ny" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for tany", () => {
+        expect(separate("tany")).toEqual({
+            syllables: [
+                { syllable: Syllable.ta, character: "ta" },
+                { syllable: Syllable.ni, character: "ny" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for nono", () => {
+        expect(separate("nono")).toEqual({
+            syllables: [
+                { syllable: Syllable.nu, character: "no" },
+                { syllable: Syllable.nu, character: "no" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for naninona", () => {
+        expect(separate("naninona")).toEqual({
+            syllables: [
+                { syllable: Syllable.na, character: "na" },
+                { syllable: Syllable.ni, character: "ni" },
+                { syllable: Syllable.nu, character: "no" },
+                { syllable: Syllable.na, character: "na" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for rano", () => {
+        expect(separate("rano")).toEqual({
+            syllables: [
+                { syllable: Syllable.ra, character: "ra" },
+                { syllable: Syllable.nu, character: "no" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for rarana", () => {
+        expect(separate("rarana")).toEqual({
+            syllables: [
+                { syllable: Syllable.ra, character: "ra" },
+                { syllable: Syllable.ra, character: "ra" },
+                { syllable: Syllable.na, character: "na" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for rariny", () => {
+        expect(separate("rariny")).toEqual({
+            syllables: [
+                { syllable: Syllable.ra, character: "ra" },
+                { syllable: Syllable.ri, character: "ri" },
+                { syllable: Syllable.ni, character: "ny" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for milalao", () => {
+        expect(separate("milalao")).toEqual({
+            syllables: [
+                { syllable: Syllable.mi, character: "mi" },
+                { syllable: Syllable.la, character: "la" },
+                { syllable: Syllable.la, character: "la" },
+                { syllable: Syllable.u, character: "o" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for fory", () => {
+        expect(separate("fory")).toEqual({
+            syllables: [
+                { syllable: Syllable.fu, character: "fo" },
+                { syllable: Syllable.ri, character: "ry" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for malagasy", () => {
+        expect(separate("malagasy")).toEqual({
+            syllables: [
+                { syllable: Syllable.ma, character: "ma" },
+                { syllable: Syllable.la, character: "la" },
+                { syllable: Syllable.ga, character: "ga" },
+                { syllable: Syllable.si, character: "sy" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for dadabe", () => {
+        expect(separate("dadabe")).toEqual({
+            syllables: [
+                { syllable: Syllable.da, character: "da" },
+                { syllable: Syllable.da, character: "da" },
+                { syllable: Syllable.be, character: "be" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for kaka", () => {
+        expect(separate("kaka")).toEqual({
+            syllables: [
+                { syllable: Syllable.ka, character: "ka" },
+                { syllable: Syllable.ka, character: "ka" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for reny", () => {
+        expect(separate("reny")).toEqual({
+            syllables: [
+                { syllable: Syllable.re, character: "re" },
+                { syllable: Syllable.ni, character: "ny" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for ray", () => {
+        expect(separate("ray")).toEqual({
+            syllables: [
+                { syllable: Syllable.ra, character: "ra" },
+                { syllable: Syllable.i, character: "y" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for zazakely", () => {
+        expect(separate("zazakely")).toEqual({
+            syllables: [
+                { syllable: Syllable.za, character: "za" },
+                { syllable: Syllable.za, character: "za" },
+                { syllable: Syllable.ke, character: "ke" },
+                { syllable: Syllable.li, character: "ly" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for mamay", () => {
+        expect(separate("mamay")).toEqual({
+            syllables: [
+                { syllable: Syllable.ma, character: "ma" },
+                { syllable: Syllable.ma, character: "ma" },
+                { syllable: Syllable.i, character: "y" },
+            ],
+        });
+    });
+
+    it("should return correct syllable for vody", () => {
+        expect(separate("vody")).toEqual({
+            syllables: [
+                { syllable: Syllable.vu, character: "vo" },
+                { syllable: Syllable.di, character: "dy" },
+            ],
+        });
+    });
+
 });
