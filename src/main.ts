80163aafbdb14d46a3823056580b76f9862981bf
NickyHariniaina <ferrissushi@gmail.com>
2026-09-06 15:50:57 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { separate } from "./parser.js";
-import { factorizeSyllableToNormalPhonetic } from "./phonetic.js";
+import {
+    factorizeSyllableToNormalPhonetic,
+} from "./phonetic.js";
 import { readJsonFile } from "./utils.js";
 
-const word = "aona";
+const word = "taona";
+const expectedPhonetic = factorizeSyllableToNormalPhonetic(separate("tona"));
 
 const syllables = separate(word);
 
 const phonetic = factorizeSyllableToNormalPhonetic(syllables);
 
-console.log(phonetic);
+const factorizedPhonetic = factorizePhonetic(phonetic);
+const expectedFactorizedPhonetic = factorizePhonetic(expectedPhonetic);
 
-// aona -> get phonetic: aona -> get probability -> aona, ahoana, aon, aoana -> search in database -> ahoana -> return ahoana
+console.log(factorizedPhonetic);
+console.log(expectedFactorizedPhonetic);
+console.log(factorizedPhonetic === expectedFactorizedPhonetic);
 
+export function factorizePhonetic(phonetic: string): string {
+    return phonetic
+        .replaceAll("hu", "u")
+        .replaceAll("ua", "o")
+        .replace(/\b(\w*)(na|no|ne|ni|nu)\b/g, "$1n")
+        .replace(/\b(\w*)(hi)\b/g, "$1")
+        .replaceAll("ia", "e")
+        .replace(/\b(\w*)(si)\b/g, "$1s")
+        .replace("u", "o")
+}
